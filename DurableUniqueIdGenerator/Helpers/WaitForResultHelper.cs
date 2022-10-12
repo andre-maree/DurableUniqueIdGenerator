82251ba31b9851a366fb5e73b1fba0b29ac1ad98
Andre Maree <andre.maree@mintgroup.net>
2022-10-12 09:45:14 +0200

    Removed usings

--- DurableUniqueIdGenerator/Helpers/WaitForResultHelper.cs
+++ DurableUniqueIdGenerator/Helpers/WaitForResultHelper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DurableUniqueIdGenerator.Helpers
+﻿namespace DurableUniqueIdGenerator.Helpers
 {
     public static class WaitForResultHelper
     {
